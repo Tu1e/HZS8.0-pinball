51de6e171e3afd408f70a148042e129e49c254aa
Tu1e <vukanradosevic16@gmail.com>
2025-12-02 01:13:42 +0100

    Basic main menu UI

---
+++ Assets/_Scripts/MainMenu/MainMenuUI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] Button playButton;
+    [SerializeField] GameObject leaderboardPanel, mainPanel;
+
+    private void FixedUpdate()
+    {
+        if(inputField == null || inputField.text.Length == 0 || inputField.text.StartsWith(" "))
+        {
+            playButton.interactable = false;
+        }
+        else
+        {
+            playButton.interactable = true;
+        }
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Leaderboard()
+    {
+        leaderboardPanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+
+}
